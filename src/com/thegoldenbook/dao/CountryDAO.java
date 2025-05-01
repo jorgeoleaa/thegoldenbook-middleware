5ac02ca3c3f4a08c0da92e5d44238fe22a37a198
Jorge <jorgeoleacasanova@gmail.com>
2025-05-01 23:13:01 +0200

    refactor: changing methods api

--- src/com/thegoldenbook/dao/CountryDAO.java
+++ src/com/thegoldenbook/dao/CountryDAO.java
@@ -7,6 +7,6 @@ import com.thegoldenbook.model.Country;
 
 public interface CountryDAO {
 	
-	public List<Country> findAll(Connection con) throws DataException;
-	public Country findById (Connection con, int id) throws DataException;
+	public List<Country> findAll(Connection con, String locale) throws DataException;
+	public Country findById (Connection con, int id, String locale) throws DataException;
 }
