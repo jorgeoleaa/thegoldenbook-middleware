e203d474056606b7a73c12422fb117a2d604be44
Jorge <jorgeoleacasanova@gmail.com>
2025-05-03 18:16:25 +0200

    refactor: rename LocalidadServiceTest to LocalityServiceTest and update methods

--- test/com/thegoldenbook/service/test/LocalityServiceTest.java
+++ test/com/thegoldenbook/service/test/LocalityServiceTest.java
@@ -9,21 +9,21 @@ import com.thegoldenbook.model.Locality;
 import com.thegoldenbook.service.LocalityService;
 import com.thegoldenbook.service.impl.LocalityServiceImpl;
 
-public class LocalidadServiceTest {
+public class LocalityServiceTest {
 
-	private static Logger logger = LogManager.getLogger(LocalidadServiceTest.class);
-	private LocalityService localidadService = null;
+	private static Logger logger = LogManager.getLogger(LocalityServiceTest.class);
+	private LocalityService localityService = null;
 
-	public LocalidadServiceTest() {
-		localidadService = new LocalityServiceImpl();
+	public LocalityServiceTest() {
+		localityService = new LocalityServiceImpl();
 	}
 
 	public void testFindAll() throws Exception{
 		logger.traceEntry("Testing FindAll...");
-		List<Locality> resultados = localidadService.findAll();
+		List<Locality> resultados = localityService.findAll("es_ES");
 
 		if(resultados.isEmpty()) {
-			logger.info("No se han encontrado resultados");
+			logger.info("There are no results");
 		}else {
 			for(Locality l : resultados) {
 				logger.info(resultados);
@@ -33,11 +33,11 @@ public class LocalidadServiceTest {
 
 	public void testFindByCodigoPostal() throws Exception{
 		logger.traceEntry("Testing findByCodigoPostal...");
-		Locality l = localidadService.findByCodigoPostal(27510);
+		Locality l = localityService.findByPostalCode("27510", "es_ES");
 		if(l != null) {
 			logger.info(l);
 		}else {
-			logger.info("No se han encontrado resultados");
+			logger.info("There are no results");
 		}
 
 
@@ -45,19 +45,19 @@ public class LocalidadServiceTest {
 
 	public void testFindById() throws Exception{
 		logger.traceEntry("Testing findByLocalidadId...");
-		Locality l = localidadService.findById(4);
+		Locality l = localityService.findById(4, "es_ES");
 		if(l != null) {
 			logger.info(l);
 		}else {
-			logger.info("No se han encontrado resultados");
+			logger.info("There are no results");
 		}
 
 	}
 
 	public static void main(String[]args) throws Exception{
-		LocalidadServiceTest test = new LocalidadServiceTest();
+		LocalityServiceTest test = new LocalityServiceTest();
 		test.testFindAll();
-		//test.testFindByCodigoPostal();
+		//test.testFindByPostalCode();
 		//test.testFindById();
 
 	}
