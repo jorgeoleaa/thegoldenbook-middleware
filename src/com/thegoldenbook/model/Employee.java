3d5ec260458930f636fdf0eef3e4c72637c860cb
Jorge <jorgeoleacasanova@gmail.com>
2025-05-03 20:35:51 +0200

    refactor: rename attributes

--- src/com/thegoldenbook/model/Employee.java
+++ src/com/thegoldenbook/model/Employee.java
@@ -7,7 +7,7 @@ public class Employee extends AbstractValueObject {
 	private String lastName;
 	private String secondLastName;
 	private String nationalId;
-	private String phone_number;
+	private String phoneNumber;
 	private String email;
 	private String password;
 	private Integer employeeTypeId;
@@ -58,12 +58,12 @@ public class Employee extends AbstractValueObject {
 		this.nationalId = nationalId;
 	}
 
-	public String getPhone_number() {
-		return phone_number;
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
 
-	public void setPhone_number(String phone_number) {
-		this.phone_number = phone_number;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
 	public String getEmail() {
