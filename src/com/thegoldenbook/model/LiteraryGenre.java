47f2f0d6184f4df1ff4c4e52d777637acde833bc
Jorge <jorgeoleacasanova@gmail.com>
2025-05-04 13:37:37 +0200

    refactor: update attributes

--- src/com/thegoldenbook/model/LiteraryGenre.java
+++ src/com/thegoldenbook/model/LiteraryGenre.java
@@ -4,6 +4,8 @@ public class LiteraryGenre extends AbstractValueObject{
 	
 	private Integer id;
 	private String name;
+	private Integer parentId;
+	private String parentName;
 	
 	public LiteraryGenre() {
 		
@@ -24,7 +26,21 @@ public class LiteraryGenre extends AbstractValueObject{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+	public Integer getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Integer parentId) {
+		this.parentId = parentId;
+	}
+
+	public String getParentName() {
+		return parentName;
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
 	
 }
