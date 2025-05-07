645950af2f6876113a258e67213893458763cd47
Jorge <jorgeoleacasanova@gmail.com>
2025-05-07 20:00:02 +0200

    feat(api) update methods in ReviewDAO interface

--- src/com/thegoldenbook/dao/ReviewDAO.java
+++ src/com/thegoldenbook/dao/ReviewDAO.java
@@ -11,13 +11,13 @@ public interface ReviewDAO {
 	public Results<Review> findByReviewCriteria (Connection con, ReviewCriteria reviewCriteria, int pos, int pageSize)
 			throws DataException;
 	
-	public Review findByReview (Connection con, Long userId, Long bookId) 
+	public Review findByReview (Connection con, Long userId, Long bookId, String locale) 
 			throws DataException;
 	
-	public Results<Review> findByUser (Connection con, Long userId, int pos, int pageSize)
+	public Results<Review> findByUser (Connection con, Long userId, String locale, int pos, int pageSize)
 			throws DataException;
 	
-	public Results<Review> findByBook (Connection con, Long bookId, int pos, int pageSize)
+	public Results<Review> findByBook (Connection con, Long bookId, String locale, int pos, int pageSize)
 			throws DataException;
 	
 	public void create (Connection con, Review review)
