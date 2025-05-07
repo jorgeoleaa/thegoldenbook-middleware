73a7d93a91f097c03faad6c8d21c156cf665b61d
Jorge <jorgeoleacasanova@gmail.com>
2025-05-07 20:41:13 +0200

    feat(literaryGenre): add findByBook and findById methods to LiteraryGenreDAO interface

--- src/com/thegoldenbook/dao/LiteraryGenreDAO.java
+++ src/com/thegoldenbook/dao/LiteraryGenreDAO.java
@@ -4,8 +4,15 @@ import java.sql.Connection;
 import java.util.List;
 
 import com.thegoldenbook.model.LiteraryGenre;
+import com.thegoldenbook.model.Subject;
 
 public interface LiteraryGenreDAO {
 	
 	public List<LiteraryGenre> findAll(Connection con, String locale) throws DataException;
+	
+	public List<Subject> findByBook(Connection con, String locale, Long libroId)
+			throws DataException;
+	
+	public Subject findById(Connection con, int id, String locale)
+			throws DataException;
 }
