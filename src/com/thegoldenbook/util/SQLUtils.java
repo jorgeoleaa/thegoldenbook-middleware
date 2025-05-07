ad413c1c3782dec36aad1b136a6bead61a9f92e8
Jorge <jorgeoleacasanova@gmail.com>
2025-05-07 23:48:14 +0200

    refactor: rename method envolverLike to wrapForLike

--- src/com/thegoldenbook/util/SQLUtils.java
+++ src/com/thegoldenbook/util/SQLUtils.java
@@ -2,7 +2,7 @@ package com.thegoldenbook.util;
 
 public class SQLUtils {
 	
-	public static final String envolverLike (String pattern){
+	public static final String wrapForLike (String pattern){
 		StringBuilder sb = new StringBuilder();
 		sb.append("%").append(pattern.toUpperCase()).append("%");
 		return sb.toString();
