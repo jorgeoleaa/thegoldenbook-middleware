475b64b87f83e4f9fb93b055d30f425acfc238cc
Jorge <jorgeoleacasanova@gmail.com>
2025-12-02 00:29:47 +0100

    feat: add Hibernate annotations to Country entity

--- src/com/thegoldenbook/model/Country.java
+++ src/com/thegoldenbook/model/Country.java
@@ -1,11 +1,28 @@
 package com.thegoldenbook.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
 public class Country extends AbstractValueObject{
-	
-	private Integer id; 
+
+    @Id
+    @Column(name = "id")
+	private Integer id;
+
+    @Column(name = "name", nullable = false, length = 45)
 	private String name;
+
+    @Column(name = "iso1", length = 5)
 	private String iso1;
+
+    @Column(name = "iso2")
 	private String iso2;
+
+    @Column(name = "phone_code")
 	private String phoneCode;
 	
 	
