f387c180c97ef231b6a54eeb1657214150815055
Jorge <jorgeoleacasanova@gmail.com>
2025-12-02 00:31:51 +0100

    refactor: update CountryDAO methods for Hibernate

--- src/com/thegoldenbook/dao/CountryDAO.java
+++ src/com/thegoldenbook/dao/CountryDAO.java
@@ -7,6 +7,6 @@ import com.thegoldenbook.model.Country;
 
 public interface CountryDAO {
 	
-	public List<Country> findAll(Connection con, String locale) throws DataException;
-	public Country findById (Connection con, int id, String locale) throws DataException;
+	public List<Country> findAll(String locale);
+	public Country findById (int id, String locale);
 }
